92d6d257cc8927b529b2f521524b70d1d101f004
 <wqjruanjian@126.com>
2021-10-28 18:21:35 +0800

    color

--- packages/pancake-uikit/src/theme/colors.ts
+++ packages/pancake-uikit/src/theme/colors.ts
@@ -51,19 +51,19 @@ export const darkColors: Colors = {
   ...baseColors,
   ...additionalColors,
   secondary: "#FDD436",
-  background: "#262C35",
-  backgroundDisabled: "#676D77",
-  backgroundAlt: "#323A4B",
+  background: "#191B2E",
+  backgroundDisabled: "#2A2D42",
+  backgroundAlt: "#20233C",
   cardBorder: "#383241",
   contrast: "#FFFFFF",
   dropdown: "#1E1D20",
   dropdownDeep: "#100C18",
-  invertedContrast: "#242A35",//弹窗背景色 card里块背景色
-  input: "#242A35",//输入框背景色
+  invertedContrast: "#191B2E",//弹窗背景色
+  input: "#191B2E",//输入框背景色
   inputSecondary: "#262130",
   tertiary: "#353547",
   text: "#FFFFFF",
-  textDisabled: "rgba(255,255,255,0.3)",
+  textDisabled: "#888D9B",
   textSubtle: "#FFFFFF",
   textDark: "#000000",
   disabled: "#524B63",

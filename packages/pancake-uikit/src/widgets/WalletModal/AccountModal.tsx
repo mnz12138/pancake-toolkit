f1860386fc63086b844bd441bcad497b78957509
 <apple>
2022-04-24 10:25:57 +0800

    1

--- packages/pancake-uikit/src/widgets/WalletModal/AccountModal.tsx
+++ packages/pancake-uikit/src/widgets/WalletModal/AccountModal.tsx
@@ -16,7 +16,7 @@ interface Props {
 const AccountModal: React.FC<Props> = ({ account, logout, onDismiss = () => null }) => (
   <Modal title="Your wallet" onDismiss={onDismiss}>
     <Text
-      fontSize="20px"
+      fontSize="12px"
       bold
       style={{ whiteSpace: "nowrap", overflow: "hidden", textOverflow: "ellipsis", marginBottom: "8px" }}
     >
